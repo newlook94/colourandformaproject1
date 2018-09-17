0f14f71770809d4d780d5370d614d37ba29dbba7
Svitlana Onats <svitlanaonats@gmail.com>
2018-09-17 12:32:16 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,60 @@
+<html> 
+<head>
+<title>Forma&Colour</title>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251"> </meta>
+<script type="text/javascript" language="javascript"> 
+</script>
+</head>
+<body>
+<img src="/img/logo+girl.jpg"  width: 100%;
+    height: 100%; border=0>
+<script type="text/javascript" language="javascript">
+<script language="JavaScript"> document.write("Это включение JavaScript") </script>
+    
+    <style type="text/css"> 
+    #50C13C body { background: #696969; 
+} 
+a { color: red;  
+} 
+a:active { color: #ffff00; 
+}
+a:visited { color: #50C13C; 
+}
+       <script language="javascript">
+imgFile = new Array()
+imgFile[0] = "/img/birds.jpg"/
+imgFile[1] = "/img/colourandforma.jpg"/
+imgFile[2] = "/img/girl.jpg"//
+imgFile[3] = "/img/glassplate.jpg"/
+imgFile[4] = "/img/glasswomen"/
+imgSrc = new Array()
+for(i=0;i<imgFile.length;i++) {
+  
+}
+</script>
+</HEAD><BODY > <script language="JavaScript" src="../../c_small.js"></script>
+ <center><font face="" size=4>Gallery</font></center>
+<TABLE border="0" cellspacing="0" width="90%"><TR><TD width="50%" align=""><A href="#picture" onmouseover="document.visible.src=imgSrc[0].src">Абу Симбел</A></TD>
+<TD width="50%" rowspan="5"><IMG border="0" name="visible" src="girl.jpg"  width="500" height="400"></TD>
+</TR><TR><TD width="50%" align=""><A href="#picture" onmouseover="document.visible.src=imgSrc[1].src">Римские катакомбы</A></TD></TR><TR><TD width="50%" align=""><A href="#picture" onmouseover="document.visible.src=imgSrc[2].src">Петра,Иордания</A></TD></TR><TR><TD width="50%" align=""><A href="#picture" onmouseover="document.visible.src=imgSrc[3].src">Колизей</A></TD></TR><TR><TD width="50%" align=""><A href="#picture" onmouseover="document.visible.src=imgSrc[4].src">Парфенон</A></TD></TR></TABLE>
+<H2> </H2>
+<P> </P><script language="JavaScript" src="../../c_small.js"></script>
+<DIV align="center">
+<script>
+<!--
+window.name = "source"
+function viewPage() {
+    view = window.open("", "view")
+    view.document.open()
+    view.document.write(document.forms[0].elements[0].value)
+    view.document.write("<BR>")
+    view.document.close()}
+ -->
+</script></DIV>
+ <script language="JavaScript" src="../../c_small.js"></script>
+<IMG src="../Image/Baalbek3.jpg" width="1" height="1" border="0"></DIV> <HR width="70%" align="center">
+<script language="JavaScript" src="../../c_small.js"></script>
+
+</style>
+</body>
+<html>
